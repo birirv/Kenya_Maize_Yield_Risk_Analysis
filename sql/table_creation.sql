c3741e22539dc17f6366649969a2bcfe8a4b3bab
birirv <84560449+birirv@users.noreply.github.com>
2026-01-13 22:54:43 +0300

    Create kenya_maize_production table with schema

--- sql/table_creation.sql
+++ sql/table_creation.sql
@@ -1,1 +1,20 @@
+DROP TABLE IF EXISTS kenya_maize_production;
 
+CREATE TABLE kenya_maize_production (
+    domain TEXT,
+    area_code_m49 INT,
+    area TEXT,
+    element_code INT,
+    element TEXT,
+    item TEXT,
+    year INT,
+    unit TEXT,
+    value NUMERIC
+);
+SELECT
+    column_name,
+    data_type,
+    ordinal_position
+FROM information_schema.columns
+WHERE table_name = 'kenya_maize_production'
+ORDER BY ordinal_position;
